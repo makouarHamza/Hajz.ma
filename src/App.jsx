a0d74672c26d6e78b6e9fea7f15f6098df14d0ab
Makouar Hamza <hamzamekoire@gmail.com>
2025-12-27 20:16:02 +0100

    start and add folders

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter } from 'react-router-dom'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+      <BrowserRouter>
+        <header>
+          
+        </header>
+      </BrowserRouter>
+    </div>
+
+    </>
+  )
+      
+}
+
+export default App
